b2e46359df07c48a21a492d860cece91514a3ff5
Márcio Ferreira <marcio11042002@gmail.com>
2025-06-12 10:31:46 -0300

    Update Main.java

--- Observe/src/observe/Main.java
+++ Observe/src/observe/Main.java
@@ -3,24 +3,24 @@ package observe;
 public class Main {
     public static void main(String[] args) {
 
-        SujeitoConcreto sujeito = new SujeitoConcreto(); // objeto observado
+        SujeitoConcreto sujeito = new SujeitoConcreto(); 
 
-        Observador o1 = new ObservadoConcreto(sujeito);  // objeto observador
-        Observador o2 = new ObservadoConcreto(sujeito);  // objeto observador
+        Observador o1 = new ObservadoConcreto(sujeito);  
+        Observador o2 = new ObservadoConcreto(sujeito);  
 
-        sujeito.cadastra(o1); // cadastro do observador 1
-        sujeito.cadastra(o2); // cadastro do observador 2
-        sujeito.remove(o2); // remove o segundo observador
+        sujeito.cadastra(o1); 
+        sujeito.cadastra(o2); 
+        
+        sujeito.remove(o2); 
         
         sujeito.setInformacao("PROMOÇÃO: notebook gamer R$ 4.000,00");
-        sujeito.notifica(); // notifica todos os observadores
-
+        sujeito.notifica(); 
+        
         sujeito.setInformacao("PROMOÇÃO: notebook gamer R$ 3.200,00");
-        sujeito.notifica(); // nova notificação
+        sujeito.notifica(); 
 
-        sujeito.remove(o2); // remove o segundo observador
 
         sujeito.setInformacao("PROMOÇÃO: notebook gamer R$ 2.900,00");
-        sujeito.notifica(); // agora só o o1 receberá
+        sujeito.notifica(); 
     }
 }
